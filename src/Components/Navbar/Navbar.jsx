62f20fcf6e140c687dbe214ddf97bd13f71290d2
leormix <nazariy3110@gmail.com>
2026-02-05 19:28:01 +0200

    search working normally

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,20 @@ import Dropdown_menu from '../DropDown_menu/DropDown_menu'
 import Hamburger from '../Hamburger/Hamburger'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { useSearchLogic } from './SearchLogic/SearchLogic'
+import { products } from '../../data/products'
 
 export default function Navbar({ cartCount }) {
     const [activeMenu, setActiveMenu] = useState(null)
 
+    const {
+        searchText,
+        updateSearchText,
+        suggestions
+    } = useSearchLogic(products);
+
     return (
         <header className="site-header">
-            <div className="top-bar"></div>
 
             <div className="main-bar">
 
@@ -31,9 +38,35 @@ export default function Navbar({ cartCount }) {
 
                 {/* ACTIONS */}
                 <div className="actions">
-                    <div className="search-box">
-                        <input type="text" placeholder="Search" />
+                    <div className="search-box" style={{ position: 'relative' }}>
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            value={searchText}
+                            onChange={(e) => updateSearchText(e.target.value)}
+                        />
                         <button>🔍</button>
+
+                        {/* Search */}
+
+                        {suggestions.length > 0 && (
+                            <div className="search-dropdown">
+                                {suggestions.map((product) => (
+                                    <Link
+                                        to={`/product/${product.id}`}
+                                        key={product.id}
+                                        className="search-item"
+                                        onClick={() => updateSearchText('')}
+                                    >
+                                        <img src={product.image} alt={product.name} />
+                                        <div className="search-item-info">
+                                            <span className="search-name">{product.name}</span>
+                                            <span className="search-price">${product.price}</span>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </div>
+                        )}
                     </div>
 
                     <div className="icons">
